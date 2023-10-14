6563b0e6b0b7222dd7b1ec9f995b7a3f5f6f9c90
ibarguen-dev <jt158161@gmail.com>
2023-10-13 21:52:44 -0500

    se realiza comentarios al bot

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+# se instacai la libreria de whatsapp
 import pywhatkit
-
+# se intacia la libreria de de fecha
 from datetime import datetime
+# se instacia la libreria de tiempo
 import time
 
+#se crea una variable que tenga la fecha y hora + 60 para que tenga un minuto para complectar el proceso
 seconds = time.time() + 60
 
+#se crea una varibale para darle formato de fecha y hora 
 data = datetime.fromtimestamp(seconds)
 
-pywhatkit.sendwhatmsg("+573206387022","Buenos dias caro como estas",data.hour,
-                      data.minute)
+# se crea un objecto de mensaje con los siguentes parametros
+#numero mas indicativo
+#mensaje
+#hora
+#minuto
+pywhatkit.sendwhatmsg("+57xxxxxxxxxxx","mensaje ",data.hour,data.minute)
+
+# se crea un tiempo de espera de 5 segundos
 time.sleep(5)
 
-# pywhatkit.sendwhats_image("+573127251557","test.png")
+#se crea un objecto de para enviar fotos
+pywhatkit.sendwhats_image("+57xxxxxxxxxxx","img.jpg")
